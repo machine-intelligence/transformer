481901869ed0a2f8733ab5b439582897f1bb6721
Max Harms <raelifin@gmail.com>
2017-09-11 12:51:00 -0700

    PEP8

--- prepro.py
+++ prepro.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-#/usr/bin/python2
+# /usr/bin/python2
 '''
-June 2017 by kyubyong park. 
+June 2017 by kyubyong park.
 kbpark.linguist@gmail.com.
 https://www.github.com/kyubyong/transformer
 '''
@@ -16,18 +16,19 @@ from collections import Counter
 
 def make_vocab(fpath, fname):
     '''Constructs vocabulary.
-    
+
     Args:
       fpath: A string. Input file path.
       fname: A string. Output file name.
-    
+
     Writes vocabulary line by line to `preprocessed/fname`
-    '''  
+    '''
     text = codecs.open(fpath, 'r', 'utf-8').read()
     text = regex.sub("[^\s\p{Latin}']", "", text)
     words = text.split()
     word2cnt = Counter(words)
-    if not os.path.exists('preprocessed'): os.mkdir('preprocessed')
+    if not os.path.exists('preprocessed'):
+        os.mkdir('preprocessed')
     with codecs.open('preprocessed/{}'.format(fname), 'w', 'utf-8') as fout:
         fout.write("{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n".format("<PAD>", "<UNK>", "<S>", "</S>"))
         for word, cnt in word2cnt.most_common(len(word2cnt)):
