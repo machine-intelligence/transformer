52e1e5bfb6a38f31a07cb8a54a03fa7e3c7cda87
James Payor <james@payor.com.au>
2017-09-12 14:03:37 -0700

    Refactor drawing a bit. Cleanup, small drawing improvements.

--- eval.py
+++ eval.py
@@ -19,6 +19,63 @@ from train import Graph
 from nltk.translate.bleu_score import corpus_bleu
 from PIL import Image, ImageDraw, ImageFont
 
+batches_to_visualize = 1
+
+def visualizeEncoderAttention(sources, tensors_of_interest, batch_index: int):
+    x_step_size = 100
+    y_step_size = 100
+    colors = ((0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 0, 255), (0, 180, 255), (200, 200, 200), (255, 100, 0), (100, 0, 255))
+    assert len(colors) == hp.num_heads
+    attention_strength_threshold = 0.3
+
+    for sentence_index, sentence in enumerate(sources):
+        print("Source sentence:", sentence)
+
+        im = Image.new('RGB', (x_step_size * (hp.maxlen + 1), y_step_size * hp.num_blocks), color=(255, 255, 255, 255))
+        draw = ImageDraw.Draw(im, 'RGBA')
+
+        # If you're not on paperspace, you get to have nicer text with the following:
+        # noinspection PyBroadException
+        try:
+            font = ImageFont.truetype("arial.ttf", 12)
+            draw.font = font
+        except:
+            pass
+
+        for layer in range(hp.num_blocks):
+            attn_signal_strength = tensors_of_interest['Attention-Signal-Strength%s' % layer]
+            residual_signal_strength = tensors_of_interest['Residual-Signal-Strength%s' % layer]
+            activation = tensors_of_interest['Activation%s' % layer]
+
+            for head_index in range(hp.num_heads):
+                for q_index in range(hp.maxlen):
+                    for k_index in range(hp.maxlen):
+                        color = colors[head_index]
+                        attention_strength = activation[head_index * hp.batch_size + sentence_index][q_index][k_index]
+                        if attention_strength > attention_strength_threshold:
+                            draw.line(
+                                (((q_index + 1) * x_step_size, im.size[1] - (layer + 1) * y_step_size),
+                                 ((k_index + 1) * x_step_size, im.size[1] - layer * y_step_size)),
+                                fill=color + (int(255 * attention_strength),),
+                                width=3)
+
+                    residual_variance = residual_signal_strength[sentence_index][q_index] ** 2
+                    attention_variance = attn_signal_strength[sentence_index][q_index] ** 2
+                    # print("Variances:", residual_variance, attention_variance)
+                    strength_ratio = residual_variance / (residual_variance + attention_variance)
+                    scale = strength_ratio * 10.0
+
+                    dot_x = (q_index + 1) * x_step_size
+                    dot_y = im.size[1] - (layer + 1) * y_step_size
+                    draw.ellipse((dot_x - scale, dot_y - scale, dot_x + scale, dot_y + scale), fill=(0, 0, 0, 127))
+
+        for i, word in enumerate(sentence.split()):
+            text_size_x, text_size_y = draw.textsize(word)
+            draw.text((((i + 1) * x_step_size) - text_size_x / 2.0, im.size[1] - 20), text=word, fill=(0, 0, 0, 255))
+
+        del draw
+        im.save("fig/Activation-Batch-{}-Sentence-{}.png".format(batch_index, sentence_index), "PNG")
+
 def eval():
     # Load graph
     g = Graph(is_training=False)
@@ -28,8 +85,6 @@ def eval():
     X, Sources, Targets = load_test_data()
     en2idx, idx2en = load_en_vocab()
     de2idx, idx2de = load_de_vocab()
-
-#     X, Sources, Targets = X[:33], Sources[:33], Targets[:33]
 
     # Start session
     with g.graph.as_default():
@@ -64,58 +119,10 @@ def eval():
 
                         print([idx2de[idx] for idx in preds[0]])
 
-                        if j == 0:
-                            for sentence_idx in range(len(sources)):
-                                print("Source:", sources[sentence_idx])
-                                print("Target:", targets[sentence_idx])
-
-                                x_step_size = 100
-                                y_step_size = 100
-                                colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 0, 255), (0, 180, 255), (200, 200, 200), (255, 100, 0), (100, 0, 255)]
-                                im = Image.new('RGB', (x_step_size * (hp.maxlen + 1), y_step_size * hp.num_blocks), color=(255, 255, 255, 255))
-                                draw = ImageDraw.Draw(im, 'RGBA')
-                                # font = ImageFont.truetype("arial.ttf", 15)
-                                # draw.font = font
-
-                                tensor_keys = [None] + list(g.tensors_of_interest.keys())  # Add a null key at the start so it lines up with the tensors_out list
-                                for layer in range(hp.num_blocks):
-                                    attn_signal_strength = tensors_out[tensor_keys.index('Attention-Signal-Strength%s' % layer)]
-                                    residual_signal_strength = tensors_out[tensor_keys.index('Residual-Signal-Strength%s' % layer)]
-
-                                    activation = tensors_out[tensor_keys.index('Activation%s' % layer)]
-                                    for q_index in range(hp.maxlen):
-                                        for k_index in range(hp.maxlen):
-                                            for head_index in range(hp.num_heads):
-                                                color = colors[head_index]
-                                                strength = activation[sentence_idx + head_index * hp.batch_size][q_index][k_index]
-                                                if strength > 0.3:
-                                                    draw.line(
-                                                        ((q_index + 1) * x_step_size,
-                                                         im.size[1] - (layer + 1) * y_step_size,
-                                                         (k_index + 1) * x_step_size,
-                                                         im.size[1] - layer * y_step_size),
-                                                        fill=color + (int(255 * strength),),
-                                                        width=3)
-
-                                        residual_variance = residual_signal_strength[sentence_idx][q_index][0] ** 2
-                                        attention_variance = attn_signal_strength[sentence_idx][q_index][0] ** 2
-                                        # print("Variances:", residual_variance, attention_variance)
-                                        strength_ratio = residual_variance / (residual_variance + attention_variance)
-                                        scale = strength_ratio * 10.0
-                                        # print(strength_ratio)
-                                        dot_x = (q_index + 1) * x_step_size
-                                        dot_y = im.size[1] - (layer + 1) * y_step_size
-                                        draw.ellipse((dot_x - scale, dot_y - scale, dot_x + scale, dot_y + scale), fill=(0, 0, 0, 127))
-
-                                for i, word in enumerate(sources[sentence_idx].split()):
-                                    text_size_x, text_size_y = draw.textsize(word)
-                                    draw.text((((i + 1) * x_step_size) - text_size_x / 2.0, im.size[1] - text_size_y), text=word, fill=(0, 0, 0, 255))
-
-                                del draw
-                                im.save("%s-Activation.png" % sentence_idx, "PNG")
-
-                    # print(g.tensors_of_interest)
-                    return
+                        # For the first few batches, we save figures giving the attention structure in the encoder.
+                        if j == 0 and i < batches_to_visualize:
+                            tensor_keys = [None] + list(g.tensors_of_interest.keys())  # Add a null key at the start so it lines up with the tensors_out list
+                            visualizeEncoderAttention(sources=sources, tensors_of_interest={key: value for key, value in zip(tensor_keys, tensors_out)}, batch_index=i)
 
                     # Write to file
                     for source, target, pred in zip(sources, targets, preds):  # sentence-wise
--- modules.py
+++ modules.py
@@ -252,9 +252,9 @@ def multihead_attention(queries,
         outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=2)  # (N, T_q, C)
 
         # Store the relative strengths of the output and residual signals.
-        attention_mean, attention_variance = tf.nn.moments(outputs, [-1], keep_dims=True)
+        attention_mean, attention_variance = tf.nn.moments(outputs, [-1], keep_dims=False)
         tensors_of_interest["Attention-Signal-Strength" + suffix] = attention_variance ** 0.5
-        residual_mean, residual_variance = tf.nn.moments(queries, [-1], keep_dims=True)
+        residual_mean, residual_variance = tf.nn.moments(queries, [-1], keep_dims=False)
         tensors_of_interest["Residual-Signal-Strength" + suffix] = residual_variance ** 0.5
 
         # Residual connection
